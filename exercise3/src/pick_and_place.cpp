202856d80866ea04eb60e4c5ab7b6c8bb2734e3b
korhona24 <162049458+korhona24@users.noreply.github.com>
2024-04-25 19:28:18 +0300

    Update pick_and_place.cpp
    Lisätty mahdollisuus ottaa koordinaatit rostopicista.

--- exercise3/src/pick_and_place.cpp
+++ exercise3/src/pick_and_place.cpp
@@ -11,9 +11,11 @@
 #include <iostream>
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
+#include "std_msgs/Float64.h"
 
 #include <geometry_msgs/Transform.h>
 #include <ros/ros.h>
+
 
 int pick_and_place(int argc, char **argv, std::vector<double> pick_coords, std::vector<double> place_coords, bool reset) {
   ros::init(argc, argv, "pick_and_place");
@@ -24,9 +26,9 @@ int pick_and_place(int argc, char **argv, std::vector<double> pick_coords, std::
 
   // Wait for the simulator to come online and then we reset it
   if (reset) {
-	std_srvs::Empty srv_reset;
-	ros::service::waitForService("/lumi_mujoco/reset");
-	ros::service::call("/lumi_mujoco/reset", srv_reset);
+    std_srvs::Empty srv_reset;
+    ros::service::waitForService("/lumi_mujoco/reset");
+    ros::service::call("/lumi_mujoco/reset", srv_reset);
   }
   // Load MoveGroup interface and moveit visual tools
   moveit::planning_interface::MoveGroupInterface g_arm("lumi_arm");
@@ -86,37 +88,18 @@ int pick_and_place(int argc, char **argv, std::vector<double> pick_coords, std::
   tf::transformMsgToEigen(pick_to_base_transform.transform, grasp_pose_eigen);
   tf::transformMsgToEigen(place_to_base_transform.transform, place_pose_eigen);
 
-  
+
+  grasp_pose_eigen = state.getGlobalLinkTransform("base_link") * grasp_pose_eigen * Eigen::AngleAxisd(M_PI, Eigen::Vector3d::UnitX());
+  place_pose_eigen = state.getGlobalLinkTransform("base_link") * place_pose_eigen * Eigen::AngleAxisd(M_PI, Eigen::Vector3d::UnitX());
+  Eigen::Isometry3d pre_grasp_pose_eigen = grasp_pose_eigen * Eigen::Translation3d(0.0, 0.0, -0.1); 
+  Eigen::Isometry3d pre_place_pose_eigen = place_pose_eigen * Eigen::Translation3d(0.0, 0.0, -0.1); 
 
 
+  vis.publishAxis(grasp_pose_eigen);
+  vis.publishAxis(place_pose_eigen);
+  vis.publishAxis(pre_grasp_pose_eigen);
+  vis.publishAxis(pre_place_pose_eigen);
 
-
-  /*TODO: Compute required poses and visualize them 
-  (Hints: we show two examples here. (Use this as reference)
-    - e1 represent for translation 0.1m in X axis while
-      Eigen::Isometry3d e1 = state.getGlobalLinkTransform("base_link") * Eigen::Translation3d(0.1, 0.0, 0.0);
-	  
-	- e2 represents for translation -0.1m in X axis and rotation PI/2 around X axis)
-      Eigen::Isometry3d e2 = state.getGlobalLinkTransform("base_link")  * Eigen::Translation3d(-0.1, 0.0, 0.0) * Eigen::AngleAxisd(M_PI_2, Eigen::Vector3d::UnitX());
-	  
-	  We already have place_pose written for you. Please use it as reference to fill in grasp_pose and pre_grasp_pose
-      // Hints: you can use this function to visualize the frames/ pose. 
-      vis.publishAxis(e1);
-      vis.publishAxis(e2);
-  */ 
-  // =============== Your code ===================  
-     grasp_pose_eigen = state.getGlobalLinkTransform("base_link") * grasp_pose_eigen * Eigen::AngleAxisd(M_PI, Eigen::Vector3d::UnitX());
-     place_pose_eigen = state.getGlobalLinkTransform("base_link") * place_pose_eigen * Eigen::AngleAxisd(M_PI, Eigen::Vector3d::UnitX());
-     Eigen::Isometry3d pre_grasp_pose_eigen = grasp_pose_eigen * Eigen::Translation3d(0.0, 0.0, -0.1); 
-	 Eigen::Isometry3d pre_place_pose_eigen = place_pose_eigen * Eigen::Translation3d(0.0, 0.0, -0.1); 
-
-
-     vis.publishAxis(grasp_pose_eigen);
-     vis.publishAxis(place_pose_eigen);
-     vis.publishAxis(pre_grasp_pose_eigen);
-	 vis.publishAxis(pre_place_pose_eigen);
-
-  // ==============================================
   vis.trigger();
 
   std::vector<moveit_msgs::RobotTrajectory> trajectories;
@@ -190,6 +173,7 @@ int pick_and_place(int argc, char **argv, std::vector<double> pick_coords, std::
   state.setVariablePositions(
     trajectories.back().joint_trajectory.joint_names,
     trajectories.back().joint_trajectory.points.back().positions);
+  // ==============================================
 
 
   // =============== PREPLACE to PLACE ===================
@@ -202,7 +186,7 @@ int pick_and_place(int argc, char **argv, std::vector<double> pick_coords, std::
   moveit_msgs::RobotTrajectory rtraj3;
   const double d3 = g_arm.computeCartesianPath({place_pose}, 0.01, 1.4, rtraj3);
   if (d3 < 0.99) {
-     ROS_ERROR_STREAM("Cannot interpolate to the pregrasping position");
+     ROS_ERROR_STREAM("Cannot interpolate to the place position");
      return -1;
   }
   trajectories.push_back(rtraj3);
@@ -210,6 +194,7 @@ int pick_and_place(int argc, char **argv, std::vector<double> pick_coords, std::
   state.setVariablePositions(
     trajectories.back().joint_trajectory.joint_names,
     trajectories.back().joint_trajectory.points.back().positions);
+  // ==============================================
 
 
   // =============== PLACE to PREPLACE ===================
@@ -217,7 +202,7 @@ int pick_and_place(int argc, char **argv, std::vector<double> pick_coords, std::
   moveit_msgs::RobotTrajectory rtraj4;
   const double d4 = g_arm.computeCartesianPath({pre_place_pose}, 0.01, 1.4, rtraj4);
   if (d4 < 0.99) {
-     ROS_ERROR_STREAM("Cannot interpolate to the pregrasping position");
+     ROS_ERROR_STREAM("Cannot interpolate to the preplace position");
      return -1;
   }
   trajectories.push_back(rtraj4);
@@ -225,79 +210,96 @@ int pick_and_place(int argc, char **argv, std::vector<double> pick_coords, std::
   state.setVariablePositions(
     trajectories.back().joint_trajectory.joint_names,
     trajectories.back().joint_trajectory.points.back().positions);
- 
+   // ==============================================
+
 
   // Visualise all trajectories
   for (const moveit_msgs::RobotTrajectory &t : trajectories) {
     vis.publishTrajectoryLine(t, state.getLinkModel(ee_link), jmg);
   }
+
   vis.trigger();
+  g_hand.setStartStateToCurrentState();
 
-  if (true) {
-    g_hand.setStartStateToCurrentState();
-
-    g_arm.execute(trajectoryToPlan(trajectories[0])); // Execute trajectory to PRE_GRASP
-    ros::Duration(1.0).sleep(); 
-    g_hand.execute(trajectoryToPlan(getGripperTrajectory(g_hand, true))); // OPEN GRIPPER
-    
-    ros::Duration(1.0).sleep();
-    g_arm.execute(trajectoryToPlan(trajectories[1]));
-    ros::Duration(1.0).sleep();
-    g_hand.execute(trajectoryToPlan(getGripperTrajectory(g_hand, false)));
-    ros::Duration(1.0).sleep();
-    g_arm.execute(trajectoryToPlan(trajectories[2]));
-    ros::Duration(1.0).sleep();
-    g_arm.execute(trajectoryToPlan(trajectories[3]));
-    ros::Duration(1.0).sleep();
-	g_arm.execute(trajectoryToPlan(trajectories[4]));
-	ros::Duration(1.0).sleep();
-    g_hand.execute(trajectoryToPlan(getGripperTrajectory(g_hand, true)));
-    ros::Duration(1.0).sleep();
-	g_arm.execute(trajectoryToPlan(trajectories[5]));
-    ros::Duration(1.0).sleep();
-
-  } else {
-     return -1;
-  }
+  g_arm.execute(trajectoryToPlan(trajectories[0])); // Execute trajectory to PRE_GRASP
+  ros::Duration(1.0).sleep(); 
+  g_hand.execute(trajectoryToPlan(getGripperTrajectory(g_hand, true))); // OPEN GRIPPER
+  ros::Duration(1.0).sleep();
+  g_arm.execute(trajectoryToPlan(trajectories[1]));
+  ros::Duration(1.0).sleep();
+  g_hand.execute(trajectoryToPlan(getGripperTrajectory(g_hand, false)));
+  ros::Duration(1.0).sleep();
+  g_arm.execute(trajectoryToPlan(trajectories[2]));
+  ros::Duration(1.0).sleep();
+  g_arm.execute(trajectoryToPlan(trajectories[3]));
+  ros::Duration(1.0).sleep();
+  g_arm.execute(trajectoryToPlan(trajectories[4]));
+  ros::Duration(1.0).sleep();
+  g_hand.execute(trajectoryToPlan(getGripperTrajectory(g_hand, true)));
+  ros::Duration(1.0).sleep();
+  g_arm.execute(trajectoryToPlan(trajectories[5]));
+  ros::Duration(1.0).sleep();
 
 
   return 0;
 }
+
+// Global cube coordinates from OptiTrack (or rostopic)
+double x;
+double y;
+double z;
+void messageX(const std_msgs::Float64::ConstPtr &msg) {
+    x = msg->data;
+}
+void messageY(const std_msgs::Float64::ConstPtr &msg) {
+    y = msg->data;
+}
+void messageZ(const std_msgs::Float64::ConstPtr &msg) {
+    z = msg->data;
+}
+
 int main(int argc, char **argv) {
-	//std::vector<double> pick_coords({0.55, 0.0, 0.025});
-	//std::vector<double> place_coords({0.0, -0.5, 0.025});
-	//pick_and_place(argc, argv, pick_coords, place_coords, true);
-	double pick_x;
-	double pick_y;
-	double pick_z;
-	double place_x;
-	double place_y;
-	double place_z;
-	bool reset = true;
-    bool continue_;
-	std::cout << "Continue?" << std::endl;
-	std::cin >> continue_;
-	while (continue_) {
-		std::cout << "Enter pick coordinate x?" << std::endl;
-		std::cin >> pick_x;
-		std::cout << "Enter pick coordinate y?" << std::endl;
-		std::cin >> pick_y;
-		std::cout << "Enter pick coordinate z?" << std::endl;
-		std::cin >> pick_z;
-		std::cout << "Enter place coordinate x?" << std::endl;
-		std::cin >> place_x;
-		std::cout << "Enter place coordinate y?" << std::endl;
-		std::cin >> place_y;
-		std::cout << "Enter place coordinate z?" << std::endl;
-		std::cin >> place_z;
+	ros::init(argc, argv, "pick_and_place");
+  ros::NodeHandle nodeHandler;
+  ros::Subscriber sub1 = nodeHandler.subscribe<std_msgs::Float64>("/x", 2, messageX);
+  ros::Subscriber sub2 = nodeHandler.subscribe<std_msgs::Float64>("/y", 2, messageY);
+  ros::Subscriber sub3 = nodeHandler.subscribe<std_msgs::Float64>("/z", 2, messageZ);
+  ros::AsyncSpinner spinner(2);
+  spinner.start();
+	double pick_x, pick_y, pick_z, place_x, place_y, place_z;
+	bool reset = true;  // Reset the simulation
+  bool continue_ = true;
+  bool ros_topic_selected;
+  while (continue_) {
+    std::cout << "Press 1 to read the coordinates from ROS topic" << std::endl;
+    std::cout << "Press 0 to choose the coordinates manually" << std::endl;
+    std::cin >> ros_topic_selected;
+    std::vector<double> pick_coords;
+    if (ros_topic_selected) {
+      std::cout << "Cube detected at " << x << ", " << y << ", " << z << std::endl;
+      pick_coords = {x, y, z};
+    }
 
-		std::vector<double> pick_coords({pick_x, pick_y, pick_z});
-	    std::vector<double> place_coords({place_x, place_y, place_z});
-		pick_and_place(argc, argv, pick_coords, place_coords, reset);
+    else {  // Manual control selected
+      std::cout << "Enter pick coordinate x?" << std::endl;
+      std::cin >> pick_x;
+      std::cout << "Enter pick coordinate y?" << std::endl;
+      std::cin >> pick_y;
+      std::cout << "Enter pick coordinate z?" << std::endl;
+      std::cin >> pick_z;
+      pick_coords = {pick_x, pick_y, pick_z};
+    }
 
-		std::cout << "Continue?" << std::endl;
-		std::cin >> continue_;
-		reset = false;
-	}
-
+    std::cout << "Enter place coordinate x?" << std::endl;
+    std::cin >> place_x;
+    std::cout << "Enter place coordinate y?" << std::endl;
+    std::cin >> place_y;
+    std::cout << "Enter place coordinate z?" << std::endl;
+    std::cin >> place_z;
+    std::vector<double> place_coords({place_x, place_y, place_z});
+    pick_and_place(argc, argv, pick_coords, place_coords, reset);
+    std::cout << "Press 1 to continue, or 0 to quit" << std::endl;
+    std::cin >> continue_;
+    reset = false;
+  }
 }
